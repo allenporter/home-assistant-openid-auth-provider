0cd9c9b5e3559796560cc91dc6381440538e500c
Allen Porter <allen.porter@gmail.com>
2024-12-07 05:52:57 +0000

    Remove unused imports

--- tests/test_init.py
+++ tests/test_init.py
@@ -2,17 +2,9 @@
 
 import pytest
 
-from homeassistant import config_entries
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers import device_registry as dr
 
 from pytest_homeassistant_custom_component.common import (
     MockConfigEntry,
-    async_mock_service,
-)
-
-from custom_components.openid_auth_provider.const import (
-    DOMAIN,
 )
 
 
